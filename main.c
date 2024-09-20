4cda50776afa777dc74d6a792706e18ca7748a56
oussama0ozirus <elkhairioussama0+github@gmail.com>
2024-09-20 11:43:24 +0100

    update

--- main.c
+++ main.c
@@ -1,18 +1,40 @@
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <string.h>
 
 typedef struct users {
     int id;
-    char password[50];
-    char role[10];
+    char password[15];
+    char role[15];
+    struct users *next;
 } users;
 
+users* func_create_node(users *p) {
+
+    // alocate memory for a new node.
+    users *tmp = (users *)malloc(sizeof(users));
+    if (!tmp) {
+        printf("Memory allocation has failed!");
+        return NULL;
+    }
+    
+
+}
 
 int main() {
     int option;
-    int id_found;       //id_valid
-    int pass_found;     //pass_valid
-    int num_of_attempts; 
+    int id_found;  
+    int pass_found;    
+    int num_of_attempts;
+    users *ptr_head;
+
+    // initializing variables
+    option = 0;
+    id_found = 0;
+    pass_found = 0;
+    num_of_attempts = 3;
+    ptr_head = NULL;
 
     // The first menu display
     printf("\tPlease Enter a valid number option from the list bellow:\n");
@@ -21,13 +43,15 @@ int main() {
     printf("\t\t3-> Exit.\n\n");
     printf("\t\tEnter your option here:   ");
     
-    // Take the input
+    // Take the input from the user
     scanf("%d", &option);
+
     printf("\n\n\n");
 
     switch (option) {
         case (1):
-            
+            if (!(ptr_head = func_create_add_node(ptr_head))) return -1;
+
     }
     return 0;
 }
