2c7cd8e7881dfa2e81ac6dc38a10d1cbcc41fe63
oussama0ozirus <elkhairioussama0+github@gmail.com>
2024-09-20 10:44:50 +0100

    first commit today

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
 
-
-
-
 #include <stdio.h>
+
+typedef struct users {
+    int id;
+    char password[50];
+    char role[10];
+} users;
 
 
 int main() {
+    int option;
+    int id_found;       //id_valid
+    int pass_found;     //pass_valid
+    int num_of_attempts; 
 
+    // The first menu display
+    printf("\tPlease Enter a valid number option from the list bellow:\n");
+    printf("\t\t1-> Sign up.\n");
+    printf("\t\t2-> Sign in.\n");
+    printf("\t\t3-> Exit.\n\n");
+
+    // Take the input
+    scanf("%d", option);
 
     return 0;
 }
